83525ba5b3470ca7b3f2b14d015ea9b7f35cefee
barcsgergo <36512890+barcsgergo@users.noreply.github.com>
2018-05-06 17:06:30 +0200

    Add files via upload

---
+++ 8as.py
@@ -0,0 +1,10 @@
+
+#két for ciklussal el kezdi bejárni a 2 szót
+
+#ha talál 1 közös betűt a 2 szóban akkor csinál egy új listát és belepakolja azt a betűt
+#ezután megnézi, hogy a 2 szóban a következő betű is azonos-e, ha igen bele rakja azt is ugyanabba a listába
+#ha nem, akkor a listát valahogy eltárolja
+#majd lépeget tovább a szón amíg megint egy közös betűt nem talál mindkettőben, ekkor egy új listába fog belepakolni
+#így sok listát fogunk kapni, ezeknek a méretét valahogy egymáshoz kellene mérni és a legnagyobbat kiiratni
+
+
