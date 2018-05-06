df184423236de51185a860072fa9d28ee15c9caa
barcsgergo <36512890+barcsgergo@users.noreply.github.com>
2018-05-06 15:46:46 +0200

    Add files via upload

--- 12es.py
+++ 12es.py
@@ -1,4 +1,22 @@
+#csinálok először egy prim fv-t, ami mindig meg fogja vizsgálni a későbbiekben, hogy prím-e az aktuális szám
 
+#Végig menni a számokon egészen addig amíg 10001. prímet meg nem találtuk
+
+#a feladatból tudjuk, az első 6 prímet és az aktuális számot, így elég attól a számtól indítani a ciklust
+#amig a hany változó nem éri el a 10001-et addig a szam változóhoz hozzá adunk 1-et és megvizsgáljuk, hogy prím-e
+#ha igen, akkor a hany-hoz hozzá adunk 1-et
+#ha a hany 10001 kiiratni a számot
+
+#jól működik a program és kiírja, hogy 104743 viszont nagyon lassan fut (~40-50 másodperc)
+
+def prim_e(n):
+    if n==2:
+            return 'Prim'
+
+    for i in range(2,n):
+        if n%i == 0:
+            return 'Nem Prim'
+    return 'Prim'
 
 
 hany=6
@@ -7,17 +25,11 @@ szam=13
 while hany != 10001:
 
     szam+=1
-    osztokszama=0
+    if prim_e(szam)=='Prim':
+        hany+=1
 
-    for i in range(1,szam+1):
-        if szam%i==0:
-            osztokszama+=1
 
-        if osztokszama==2:
-            hany+=1
-
-if hany==10001:
-    print(szam)
+print(szam)
 
 
 
@@ -25,7 +37,7 @@ if hany==10001:
 
 
 
-#Végig menni a számokon egészen addig amíg 10001. prímet meg nem találtuk
-#megszámolni, hogy az adott számnak hány osztója van
-#ha pontosan 2 db osztója van megnövelni a prímek számát 1-el
-#ha a prímek száma 10001 returnolni a számot
+
+
+
+
