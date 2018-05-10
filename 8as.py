ef15359eefde4ac68d1d5e4ed63887ab28108e56
barcsgergo <36512890+barcsgergo@users.noreply.github.com>
2018-05-10 23:16:11 +0200

    Add files via upload

--- 8as.py
+++ 8as.py
@@ -1,10 +1,17 @@
 
-#két for ciklussal el kezdi bejárni a 2 szót
-
-#ha talál 1 közös betűt a 2 szóban akkor csinál egy új listát és belepakolja azt a betűt
-#ezután megnézi, hogy a 2 szóban a következő betű is azonos-e, ha igen bele rakja azt is ugyanabba a listába
-#ha nem, akkor a listát valahogy eltárolja
-#majd lépeget tovább a szón amíg megint egy közös betűt nem talál mindkettőben, ekkor egy új listába fog belepakolni
-#így sok listát fogunk kapni, ezeknek a méretét valahogy egymáshoz kellene mérni és a legnagyobbat kiiratni
 
 
+def lcs(a,b):
+
+    if not a or not b:
+        return ''
+
+    x, xs, y, ys = a[0], a[1:], b[0], b[1:]
+
+    if x == y:
+        return x + lcs(xs, ys)
+    else:
+        return max(lcs(a, ys), lcs(xs, b), key=len)
+
+
+print(lcs('valami','lamiamidrmekmf'))
